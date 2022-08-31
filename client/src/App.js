5dbc1b1adf470287e9cd3b372e0fa27ac6ccce4f
Gabriel Norberto <sn_gabriel@outlook.com>
2022-08-31 12:38:59 -0300

    under development

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import Axios from 'axios';
 
 const BASE_URL = 'http://localhost:3001'; //Backend URL
@@ -11,41 +11,54 @@ function App() {
    const [position, setPosition] = useState('');
    const [wage, setWage] = useState(0);
 
+   const [employeeList, setEmployeeList] = useState([]);
+
    const addEmployee = () => {
       Axios.post(
-         BASE_URL+'/create', 
+         BASE_URL + '/create',
          { name, age, country, position, wage }
       ).then(() => {
          console.log("Success");
       })
    }
 
+   const getEmployees = () => {
+      Axios.get(BASE_URL+'/emlooyees').then(() => {
+         console.log("Success");
+      });
+   }
+
    return (
       <div className="App">
          <div className="information">
             <label>
-               Nome: <br/>
+               Nome: <br />
                <input type="text" placeholder="Insira seu nome" onChange={(e) => setName(e.target.value)} />
             </label>
             <label>
-               Idade: <br/>
+               Idade: <br />
                <input type="number" placeholder="Insira sua idade" onChange={(e) => setAge(e.target.value)} />
             </label>
             <label>
-               País: <br/>
-               <input type="text" placeholder="Insira seu país" onChange={(e) => setCountry(e.target.value)}/>
+               País: <br />
+               <input type="text" placeholder="Insira seu país" onChange={(e) => setCountry(e.target.value)} />
             </label>
             <label>
-               Cargo: <br/>
-               <input type="text" placeholder="Insira seu cargo" onChange={(e) => setPosition(e.target.value)}/>
+               Cargo: <br />
+               <input type="text" placeholder="Insira seu cargo" onChange={(e) => setPosition(e.target.value)} />
             </label>
             <label>
-               Salário anual: <br/>
-               <input type="number" placeholder="Insira seu salário anual" onChange={(e) => setWage(e.target.value)}/>
+               Salário anual: <br />
+               <input type="number" placeholder="Insira seu salário anual" onChange={(e) => setWage(e.target.value)} />
             </label>
          </div>
 
          <button onClick={addEmployee}>Adicionar empregado</button>
+         <hr />
+         <div className="employees">
+            <button>Listar empregados</button>
+         </div>
+
       </div>
    );
 }
